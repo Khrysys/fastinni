a0148842f5d859db47867ea9c1741749be6ed4c3
Michael Piccirillo <70709374+Khrysys@users.noreply.github.com>
2024-02-01 17:41:12 -0500

    A little bit of cleanup + some simplification & convinient enums

--- api/account/login.py
+++ api/account/login.py
@@ -5,9 +5,19 @@ from fastapi_csrf_protect import CsrfProtect
 app = APIRouter(prefix='/login', tags=['Login'])
 
 @app.get("/")
-async def get_account_login_info():
-    pass
+# If additional form security is required it should be put here, additionally request and browser verification
+# should be verified here
+async def get_account_login_info(request: Request):
+    # We don't want two accounts to be logged in (although this can be done by adjusting the JWT styles)
+    if request.cookies.get('login_jwt') is not None:
+        return 400
 
 @app.post('/')
 async def attempt_account_login(request: Request, csrf_protect:CsrfProtect = Depends()):
     await csrf_protect.validate_csrf(request)
+    
+    tag = request.get('tag')
+    password = request.get('password')
+
+
+
--- api/db/user.py
+++ api/db/user.py
@@ -1,3 +1,4 @@
+from enum import StrEnum
 from os import getenv
 from typing import Optional
 from jwt import encode
@@ -9,14 +10,33 @@ from . import engine
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+class ViewScope(StrEnum):
+    public = 'public'
+    related = 'related'
+    friends = 'friends'
+    private = 'private'
+    
+
 class User(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True)
 
+    # User information
+    display_name: str
+    tag: str
+    email: str
+
+    # Possible login secret info that gets verified
+    google_id: Optional[int]
+    password_hash: Optional[str]
+
+    # Toggles
     is_banned: Optional[bool] = Field(default=False)
 
-    display_name: str
-
-    tag: str
+    # View Modes
+    profile_view: Optional[ViewScope] = Field(default=ViewScope.public)
+    email_view: Optional[ViewScope] = Field(default=ViewScope.private)
+    phone_view: Optional[ViewScope] = Field(default=ViewScope.private)
+    integrations_view: Optional[ViewScope] = Field(default=ViewScope.private)
 
     @staticmethod
     def is_tag_available(tag: str) -> bool:
@@ -25,7 +45,6 @@ class User(SQLModel, table=True):
             result = session.exec(statement)
             return len(result.all()) is 0
 
-    password_hash: Optional[str]
 
     @property
     def password(self):
@@ -38,9 +57,6 @@ class User(SQLModel, table=True):
     def check_password(self, val) -> bool:
         return pwd_context.verify(val, self.password_hash)
 
-    
-    google_id: Optional[int]
-
     def generate_login_jwt(self, **kwargs) -> str:
         return encode({
             "id": self.id, 
@@ -49,7 +65,22 @@ class User(SQLModel, table=True):
             "google_id": self.google_id,
             "args": kwargs
         }, getenv("LOGIN_SECRET")) # type: ignore
-        
     
-            
 
+    @staticmethod
+    def try_login_user(*, tag: str, password: Optional[str] = None, google_id: Optional[str] = None) -> Optional["User"]:
+        with Session(engine) as session:
+            statement = select(User).where(User.tag==tag)
+            if password is not None:
+                result = session.exec(statement)
+                user: User = result.one()
+                return user if user.check_password(password) else None
+            
+            elif google_id is not None:
+                statement = statement.where(User.google_id==google_id)
+                result = session.exec(statement)
+                user: User = result.one()
+                return user
+            
+            else:
+                return None
