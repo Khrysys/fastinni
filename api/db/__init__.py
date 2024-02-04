5d803fa5fded03dc9057ed254980d14cfd70c648
Michael Piccirillo <70709374+Khrysys@users.noreply.github.com>
2024-02-03 23:04:30 -0500

    Add a default DB_URL

--- api/db/__init__.py
+++ api/db/__init__.py
@@ -1,7 +1,9 @@
 from os import getenv
 from sqlmodel import SQLModel, create_engine
 
-engine = create_engine(getenv("DB_URL")) # type: ignore
+db_url = getenv("DB_URL", 'postgresql+psycopg://postgres:postgres@127.0.0.1:5432/db-fastinni')
+
+engine = create_engine(getenv("DB_URL", 'postgresql+psycopg://postgres:postgres@127.0.0.1:5432/db-fastinni'))
 
 from .user import User
 
